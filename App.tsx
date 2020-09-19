aa057b879a3e7e492d46e86d5e9648319fa418dc
Jelle Versele <jelle@versele.be>
2020-09-19 18:50:28 +0200

    clean up

--- App.tsx
+++ App.tsx
@@ -1,183 +1,60 @@
 // @refresh reset
-
-import { StatusBar } from "expo-status-bar";
-import React, {
-  useState,
-  useRef,
-  useEffect,
-  useLayoutEffect,
-  ReactNode,
-  ReactChildren,
-  Ref,
-  useImperativeHandle,
-  forwardRef,
-} from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Dimensions,
-  NativeSyntheticEvent,
-  NativeScrollEvent,
-  Button,
-} from "react-native";
+import React, { useState, useRef, ReactNode } from "react";
+import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
+import { SliderAPI, Slider } from "./Slider";
 
 const sceenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
-function Slide({ color, children }: { color: string, children: ReactNode }) {
-return <View style={{ justifyContent: 'center', alignItems: 'center', width: sceenWidth, height: screenHeight, backgroundColor: color }}>{children}</View>;
-}
-
-function getDirection(index: number): "next" | "prev" | "none" {
-  if (index > 1) return "next";
-  if (index < 1) return "prev";
-  return "none";
-}
-
-function getNextIndex(currIndex: number, length: number): number {
-  const maxIndex = length - 1;
-  const nextIndex = currIndex + 1;
-
-  if (nextIndex > maxIndex) return 0;
-
-  return nextIndex;
-}
-
-function getPrevIndex(currentIndex: number, length: number) {
-  const prevIndex = currentIndex - 1;
-
-  if (prevIndex < 0) return length - 1;
-
-  return prevIndex;
-}
-
-function getNewIndex(
-  currentIndex: number,
-  length: number,
-  direction: "next" | "prev"
-): number {
-  switch (direction) {
-    case "next":
-      return getNextIndex(currentIndex, length);
-    case "prev":
-      return getPrevIndex(currentIndex, length);
-  }
-}
-
-type Api = {
-  next: () => void,
-  prev: () => void
-}
-
-function SliderComponent({
-  children,
-  width,
-  height,
-  onScroll = () => {}
-}: {
-  children: ReactNode;
-  width: number;
-  height: number;
-  onScroll?: (index: number) => void
-}, ref: Ref<Api>) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isScrolling, setIsScrolling] = useState(false);
-  const scrollView = useRef<ScrollView>(null);
-  const items = React.Children.toArray(children);
-  const totalItems = items.length;
-
-  const scrollTo = (x: number) => {
-    if (isScrolling) return;
-
-    setIsScrolling(true);
-    scrollView.current?.scrollTo({x});
-  }
-
-  useImperativeHandle(ref, () => ({
-    next: () => {
-      scrollTo(width * 2);
-    },
-    prev: () => {
-      scrollTo(0);
-    }
-  }));
-
-  const onScrollStart = () => setIsScrolling(true);
-
-  const onScrollEnd = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const index = event.nativeEvent.contentOffset.x / width;
-    const direction = getDirection(index);
-
-    if (direction === "none") { return; }
-
-    const newIndex = getNewIndex(currentIndex, totalItems, direction);
-
-    setCurrentIndex(newIndex);
-    scrollView.current?.scrollTo({ x: width * 1, animated: false });
-    setIsScrolling(false);
-
-    onScroll(newIndex);
-  };
-
-  const itemsToRender = [
-    items[getPrevIndex(currentIndex, totalItems)],
-    items[currentIndex],
-    items[getNextIndex(currentIndex, totalItems)]
-  ];
-
+function Slide({ color, children }: { color: string; children: ReactNode }) {
   return (
-    <View style={{ width, height }}>
-      <ScrollView
-        contentOffset={{x: width, y: 0}}
-        pagingEnabled
-        bounces={false}
-        ref={scrollView}
-        onMomentumScrollBegin={onScrollStart}
-        onMomentumScrollEnd={onScrollEnd}
-        horizontal
-        alwaysBounceHorizontal={false}
-        showsHorizontalScrollIndicator={false}
-      >
-        {itemsToRender.map((item, i) => (
-          <View
-            key={i}
-            style={{
-              width,
-              height,
-              alignItems: "center",
-              justifyContent: "center",
-              overflow: 'hidden'
-            }}
-          >
-            {item}
-          </View>
-        ))}
-      </ScrollView>
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        width: sceenWidth,
+        height: screenHeight,
+        backgroundColor: color,
+      }}
+    >
+      {children}
     </View>
   );
 }
 
-const Slider = forwardRef(SliderComponent);
-
 export default function App() {
-
   const [index, setIndex] = useState(0);
-  const slider = useRef<Api>(null);
+  const slider = useRef<SliderAPI>(null);
 
   return (
     <View style={styles.container}>
       <Text>{index}</Text>
-      <Slider ref={slider} onScroll={i => setIndex(i)} height={screenHeight-300} width={sceenWidth}>
-        <Slide color="orange"><Text>1</Text></Slide>
-        <Slide color="black"><Text style={{color: 'white'}}>2</Text></Slide>
-        <Slide color="yellow"><Text>3</Text></Slide>
-        <Slide color="grey"><Text>4</Text></Slide>
+      <Slider
+        ref={slider}
+        onScroll={(i) => setIndex(i)}
+        height={screenHeight - 300}
+        width={sceenWidth}
+      >
+        <Slide color="orange">
+          <Text>1</Text>
+        </Slide>
+        <Slide color="black">
+          <Text style={{ color: "white" }}>2</Text>
+        </Slide>
+        <Slide color="yellow">
+          <Text>3</Text>
+        </Slide>
+        <Slide color="grey">
+          <Text>4</Text>
+        </Slide>
       </Slider>
-      <View style={{flexDirection: 'row'}}>
-        <Button title="prev" onPress={() => slider.current?.prev()}>prev</Button>
-        <Button title="next" onPress={() => slider.current?.next()}>next</Button>
+      <View style={{ flexDirection: "row" }}>
+        <Button title="prev" onPress={() => slider.current?.prev()}>
+          prev
+        </Button>
+        <Button title="next" onPress={() => slider.current?.next()}>
+          next
+        </Button>
       </View>
     </View>
   );
---
+++ Slider.tsx
@@ -0,0 +1,145 @@
+import React, {
+  ReactNode,
+  useState,
+  useRef,
+  Ref,
+  useImperativeHandle,
+  forwardRef,
+} from "react";
+import {
+  ScrollView,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+  View,
+} from "react-native";
+
+function getDirection(index: number): "next" | "prev" | "none" {
+  if (index > 1) return "next";
+  if (index < 1) return "prev";
+  return "none";
+}
+
+function getNextIndex(currIndex: number, length: number): number {
+  const maxIndex = length - 1;
+  const nextIndex = currIndex + 1;
+
+  if (nextIndex > maxIndex) return 0;
+
+  return nextIndex;
+}
+
+function getPrevIndex(currentIndex: number, length: number) {
+  const prevIndex = currentIndex - 1;
+
+  if (prevIndex < 0) return length - 1;
+
+  return prevIndex;
+}
+
+function getNewIndex(
+  currentIndex: number,
+  length: number,
+  direction: "next" | "prev"
+): number {
+  switch (direction) {
+    case "next":
+      return getNextIndex(currentIndex, length);
+    case "prev":
+      return getPrevIndex(currentIndex, length);
+  }
+}
+
+export type SliderAPI = {
+  next: () => void;
+  prev: () => void;
+};
+
+export type Props = {
+  children: ReactNode;
+  width: number;
+  height: number;
+  onScroll?: (index: number) => void;
+};
+
+function SliderComponent(
+  { children, width, height, onScroll = () => {} }: Props,
+  ref: Ref<SliderAPI>
+) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const scrollView = useRef<ScrollView>(null);
+  const items = React.Children.toArray(children);
+  const totalItems = items.length;
+
+  const scrollTo = (x: number) => {
+    if (isScrolling) return;
+
+    setIsScrolling(true);
+    scrollView.current?.scrollTo({ x });
+  };
+
+  useImperativeHandle(ref, () => ({
+    next: () => {
+      scrollTo(width * 2);
+    },
+    prev: () => {
+      scrollTo(0);
+    },
+  }));
+
+  const onScrollStart = () => setIsScrolling(true);
+
+  const onScrollEnd = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const index = event.nativeEvent.contentOffset.x / width;
+    const direction = getDirection(index);
+
+    if (direction === "none") return;
+
+    const newIndex = getNewIndex(currentIndex, totalItems, direction);
+
+    setCurrentIndex(newIndex);
+    scrollView.current?.scrollTo({ x: width * 1, animated: false });
+    setIsScrolling(false);
+
+    onScroll(newIndex);
+  };
+
+  const itemsToRender = [
+    items[getPrevIndex(currentIndex, totalItems)],
+    items[currentIndex],
+    items[getNextIndex(currentIndex, totalItems)],
+  ];
+
+  return (
+    <View style={{ width, height }}>
+      <ScrollView
+        contentOffset={{ x: width, y: 0 }}
+        pagingEnabled
+        bounces={false}
+        ref={scrollView}
+        onMomentumScrollBegin={onScrollStart}
+        onMomentumScrollEnd={onScrollEnd}
+        horizontal
+        alwaysBounceHorizontal={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        {itemsToRender.map((item, i) => (
+          <View
+            key={i}
+            style={{
+              width,
+              height,
+              alignItems: "center",
+              justifyContent: "center",
+              overflow: "hidden",
+            }}
+          >
+            {item}
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+export const Slider = forwardRef(SliderComponent);
